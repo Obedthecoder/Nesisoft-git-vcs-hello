3a554c521432876ef95ba32bb6d7d2d82b34ffd3
Obed <obedwithpc98@gmail.com>
2026-04-10 06:30:48 +0000

    temp before removing large file

--- function.h
+++ function.h
@@ -15,27 +15,6 @@ double divide(int a, int b);
 
 int factorial(int n);
   
-=======
-int add(int a, int b) {
-    return a + b;
-}
-
-int subtract(int a, int b) {
-    return a - b;
-}
-
-int multiply(int a, int b) {
-    return a * b;
-}
-
-double divide(int a, int b) {
-    return a / b;
-}
-
-int factorial(int n) {  
-     return (n == 0) ? 1 : n * factorial(n - 1);
-}
->>>>>>> b6ded27ff1be096607cdb3b3cff852761e34e672
 #endif // FUNCTION_H
 
 
